d74fc10ddb95a1ef610e472e0fa7d93b7c157dd7
sunil540 <50818431+sunil540@users.noreply.github.com>
2025-11-27 11:13:13 +0530

    Delete config-overrides.js

--- config-overrides.js
+++
@@ -1,8 +0,0 @@
-const { override, addWebpackModuleRule } = require('customize-cra');
-
-module.exports = override(
-    addWebpackModuleRule({
-        test: /\.worker\.js$/,
-        use: { loader: 'worker-loader' }
-    })
-);
